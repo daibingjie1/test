6d2609805141c621b7e04c3820f5a69a5ae6f628
daibingjie1 <18007397627@163.com>
2019-01-28 17:32:07 +0800

    测试BOOT 打包

--- src/main/java/com/example/demo/controller/InfoController.java
+++ src/main/java/com/example/demo/controller/InfoController.java
@@ -3,14 +3,16 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/InfoController")
 public class InfoController {
 
 
     @RequestMapping("/getHa")
-    public String getHa() {
-        System.out.println("-----------------------------");
+    public String getHa(HttpServletRequest  request) {
+        System.out.println("-----------------------------"+request.getQueryString());
         return "hard";
 
     }
