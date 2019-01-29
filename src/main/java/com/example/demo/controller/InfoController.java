e75f7bed9cffe47054060d2f7f43aaa0ff6831c7
daibingjie1 <18007397627@163.com>
2019-01-29 09:20:45 +0800

    测试BOOT 打包

--- src/main/java/com/example/demo/controller/InfoController.java
+++ src/main/java/com/example/demo/controller/InfoController.java
@@ -1,10 +1,9 @@
 package com.example.demo.controller;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -14,7 +13,7 @@ public class InfoController {
 
     @RequestMapping("/getHa")
     @ResponseBody
-    public String getHa(HttpServletRequest  request, String str , JSONPObject object) {
+    public String getHa(HttpServletRequest  request, String str , JSONObject object) {
 
         System.out.println("object-----------------------------"+object);
         System.out.println("request-----------------------------"+request.getQueryString());
