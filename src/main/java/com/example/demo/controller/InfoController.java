00ccbbb83e5500f215149e04443e233a6e604170
daibingjie1 <18007397627@163.com>
2019-01-29 09:28:47 +0800

    测试BOOT 打包

--- src/main/java/com/example/demo/controller/InfoController.java
+++ src/main/java/com/example/demo/controller/InfoController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -12,9 +11,8 @@ public class InfoController {
 
 
     @RequestMapping("/getHa")
-    @ResponseBody
     public String getHa(HttpServletRequest  request, String str , JSONObject object) {
-
+        
         System.out.println("object-----------------------------"+object);
         System.out.println("request-----------------------------"+request.getQueryString());
 
@@ -22,7 +20,7 @@ public class InfoController {
             return "没有数据";
         }
 
-        return object.toString();
+        return "hard";
 
     }
 
