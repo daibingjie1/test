cb555ddbc74dddc7f206be6b3b26ea6ccefb6770
daibingjie1 <18007397627@163.com>
2019-01-29 10:25:44 +0800

    测试BOOT 打包

--- src/main/java/com/example/demo/controller/InfoController.java
+++ src/main/java/com/example/demo/controller/InfoController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @Controller
 @RequestMapping("/InfoController")
@@ -19,9 +21,21 @@ public class InfoController {
 
         System.out.println("object-----------------------------"+object);
 
-/*        if (object == null ) {
-            return "没有数据";
-        }*/
+        String param= null;
+        try {
+            BufferedReader streamReader = new BufferedReader( new InputStreamReader(request.getInputStream(), "UTF-8"));
+            StringBuilder responseStrBuilder = new StringBuilder();
+            String inputStr;
+            while ((inputStr = streamReader.readLine()) != null)
+                responseStrBuilder.append(inputStr);
+
+            JSONObject jsonObject = JSONObject.parseObject(responseStrBuilder.toString());
+            param= jsonObject.toJSONString();
+            System.out.println(param);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         return "SB";
 
